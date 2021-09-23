87d187db55e5579e7660d4e0734b0a9372259d9c
tz1201 <tz1201@nyu.edu>
2021-09-23 11:50:01 +0400

    update--2

--- animation.js
+++ animation.js
@@ -1,36 +1,69 @@
-let myVariable;
 
-myVariable = "Hello!";
+// header color change
 
-console.log(myVariable);
+let colorChangeTo = () =>{
+  document.getElementById("mouse-move").style.backgroundColor = "grey";
+};
 
-if(2+2>3){
-  console.log("haha!");
-}else{
-  console.log("it shouldn't happen");
+let colorChangeBack = () =>{
+  document.getElementById("mouse-move").style.backgroundColor = "black";
+};
+
+let headerBox_1 = document.getElementById("mouse-move");
+console.log(headerBox_1);
+headerBox_1.onmouseover = colorChangeTo;
+headerBox_1.onmouseout = colorChangeBack;
+
+let headerBox_2 = document.getElementById("mouse-move-profile");
+console.log(headerBox_2);
+headerBox_2.onmouseover = () =>{
+  document.getElementById("mouse-move-profile").style.backgroundColor = "grey";
+};
+headerBox_2.onmouseout = () =>{
+  document.getElementById("mouse-move-profile").style.backgroundColor = "black";
+};
+
+let headerBox_3 = document.getElementById("mouse-move-gallery");
+console.log(headerBox_3);
+headerBox_3.onmouseover = () =>{
+  document.getElementById("mouse-move-gallery").style.backgroundColor = "grey";
+};
+headerBox_3.onmouseout = () =>{
+  document.getElementById("mouse-move-gallery").style.backgroundColor = "black";
+};
+
+let headerBox_4 = document.getElementById("mouse-move-story");
+console.log(headerBox_4);
+headerBox_4.onmouseover = () =>{
+  document.getElementById("mouse-move-story").style.backgroundColor = "grey";
+};
+headerBox_4.onmouseout = () =>{
+  document.getElementById("mouse-move-story").style.backgroundColor = "black";
+};
+// end of header color change
+
+
+//Profile introduction
+let TinaProfile = document.getElementById("Tina-profile");
+
+function ChangeTinaProfile(){
+  document.getElementById("Tina-profile").style.fontSize='20px';
+  document.getElementById("Tina-profile").style.lineHeight='40px';
+  document.getElementById("Tina-profile").innerHTML="A NYUSH Senior<br/>From Sichuan, China<br/>Major in Data Science<br/>Minoring in IMA and Mathmatics";
 }
 
-let orangeParagraph = document.getElementById("orange-paragraph");
-orangeParagraph.addEventListener('click',() => {
-  if (orangeParagraph.style.color=="orange"){
-    orangeParagraph.style.color="black";
-  }else{
-    orangeParagraph.style.color="orange";
-  }
-})
+let YasminProfile = document.getElementById("Yasmin-profile");
 
-let greetingElement = document.getElementById("greetingElement");
-
-let makeGhostText = () =>{
-  document.getElementById("amghost").innerText = "I am a ghost";
+function ChangeYasminProfile(){
+  document.getElementById("Yasmin-profile").style.fontSize='20px';
+  document.getElementById("Yasmin-profile").style.lineHeight='40px';
+  document.getElementById("Yasmin-profile").innerHTML="A NYUAD Junior<br/>From Dubai, UAE<br/>Major in Interactive Media<br/>Minoring in Visual Arts";
 }
 
-let makeGhostTextDisappear = () =>{
-  setTimeout(()=> {
-    document.getElementById("amghost").innerText = "";
-  },5000)
+let MayaProfile = document.getElementById("Maya-profile");
+
+function ChangeMayaProfile(){
+  document.getElementById("Maya-profile").style.fontSize='20px';
+  document.getElementById("Maya-profile").style.lineHeight='40px';
+  document.getElementById("Maya-profile").innerHTML="A NYUAD Junior<br/>From South Korea<br/>Major in Computer Science<br/>Minoring in IM and Arabic";
 }
-
-greetingElement.addEventListener('mouseenter',makeGhostText)
-
-greetingElement.addEventListener('mouseleave',makeGhostTextDisappear)
